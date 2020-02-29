c4a6ce7051694e8bb88747715102193a7508138e
Rabdology <edscaife@mac.com>
2020-02-29 11:55:57 +0000

    small change

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var http = require('http');
 var socketio = require('socket.io');
 
+//const PluginManager = new require('./plugins/PluginManager')();
+
 
 var app = express();
 var server = http.Server(app);
 var websocket = socketio(server);
+
+
 
 var previous_messages = []
 
@@ -19,20 +23,23 @@ websocket.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  socket.on('JOINED', function(msg){
-    console.log('JOINED:' + msg);
+  socket.on('JOINED', function(name){
+    console.log('JOINED:' + name);
 
     //send the new person previous messages
-    for(var i = 0; i<previous_messages.length;i++){
+    for(var i = 1; i<previous_messages.length;i++){
       socket.emit('chat message',previous_messages[i])
     }
 
-    socket.emit('chat message',msg+", welcome to the chat") //send to newly connected guy
-    socket.broadcast.emit('chat message',msg+" has joined") //send to everyone else
+    socket.emit('chat message',    {name:'Server',message:name+", welcome to the chat",timestamp:new Date()}) //send to newly connected guy
+    socket.broadcast.emit('chat message',    {name:'Server',message:name+", has joined",timestamp:new Date()}) //send to everyone else
   });
   socket.on("MSSG1",function(msg){
     //print out
     console.log(msg);
+
+    //run any plugin functiolnality on tihs message
+    //msg.message = PluginManager.runChatPlugins(msg.message)
 
     //save to log of previous messages
     previous_messages = [previous_messages, msg];
