3251c3649394cb88964024cbd21eeae08a24a24b
Rabdology <edscaife@mac.com>
2020-04-29 19:29:12 +0100

    accompanying change to plugin manager

--- PluginManager.js
+++ PluginManager.js
@@ -6,29 +6,37 @@ class PluginManager  {
   chatPluginList = []
   database = []
   constructor(){
-    this.loadPlugins()
-  }
-
-  loadPlugins(){
     var data = fs.readFileSync('./plugins/plugins.xml','utf8');
     this.json = JSON.parse(parser.toJson(data, {reversible: true}));
-    var plugins = this.json['plugin_list']['plugin']
+    this.loadChatPlugins()
+    this.loadDB()
+  }
 
+  loadChatPlugins(){
+
+    var plugins = this.json['plugin_list']['chat_plugins']['plugin']
 
     for(var i=0;i<plugins.length;i++){
       var plugin = plugins[i]
       if(plugin.active){
         var pluginloc = './plugins/'+plugin.id['$t']+'/'+plugin.id['$t']+'.js'
         var plg = require(pluginloc)
-        if(plugin.type['$t']=="chat"){
+        this.chatPluginList.push(new plg())
 
-          this.chatPluginList.push(new plg())}
-
-        else if(plugin.type['$t']=="database"){this.database = new plg()}
       }
     }
 
-    console.log(this.chatPluginList)
+  console.log(this.chatPluginList)
+
+  }
+  loadDB(){
+    var db = this.json['plugin_list']['database']['plugin']
+    var dbloc = './plugins/'+db.id['$t']+'/'+db.id['$t']+'.js'
+    var database = require(dbloc)
+    this.database = new database()
+    for(var i=0;i<this.chatPluginList.length;i++){
+      if(this.chatPluginList[i].userProps!=null) this.database.addUserColumn(this.chatPluginList[i].userProps)
+    }
     console.log(this.database)
   }
 
@@ -36,7 +44,7 @@ class PluginManager  {
     for(var i = 0;i<this.chatPluginList.length;i++)
     {
       console.log(this.chatPluginList[i])
-      msg = this.chatPluginList[i].run(msg)
+      msg = this.chatPluginList[i].run(msg,this.database)
     }
     return msg
   }
