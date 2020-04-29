3a5934b1b3e8c13f76fcd9eb481229b7921b6b13
Rabdology <edscaife@mac.com>
2020-04-29 19:26:06 +0100

    work allocation updates

--- plugins/WorkAllocation/WorkAllocation.js
+++ plugins/WorkAllocation/WorkAllocation.js
@@ -4,6 +4,8 @@ class WorkAllocation {
 
   jobList = []
   numberJobs = 0
+  userProps = {"property":"jobsCompleted","default_value":0}
+
 
   constructor(){
     //load config set parameters
@@ -14,7 +16,7 @@ class WorkAllocation {
     this.jobList = config['jobList'];
     this.numberJobs = this.jobList.length;
   }
-  run(msg){
+  run(msg,db){
     try{
       if(msg.message.startsWith("wa.")){
         if(msg.message.startsWith("wa.joblist")){
@@ -32,18 +34,27 @@ class WorkAllocation {
         else if(msg.message.startsWith("wa.addjob")){
           this.numberJobs = this.numberJobs+1;
           var description = msg.message.substring(9)
-          this.jobList.push({id:this.numberJobs,description:description,assignee:"None"})
+          this.jobList.push({description:description,assignee:"None"})
 
           msg.message = "\n"+JSON.stringify(this.jobList)+"\n";
           msg.name = "Server";
         }
         else if(msg.message.startsWith("wa.complete")){
           var jobIndex = parseInt(msg.message.substring(11));
-          if(this.jobList.assignee==msg.name){
+          if(this.jobList[jobIndex].assignee==msg.name){
             this.jobList.splice(jobIndex,jobIndex);
+            msg.message = "\n"+JSON.stringify(this.jobList)+"\n";
+
+            var usr = db.getUser(msg.name);
+            usr.row.jobsCompleted += 1;
+            db.updateUser(usr);
+
           }
-          msg.message = "\n"+JSON.stringify(this.jobList)+"\n";
-          //fix bug with ids
+          else{
+            msg.message = "This job is not assigned to you"
+          }
+
+
         }
         else{
           msg.message = "invalid work allocation command"
